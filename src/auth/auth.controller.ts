46974300f606bf9bc24396d9ad84d85409baa552
Brisstone <okolijohnson69@gmail.com>
2026-08-12 23:04:00 +0100

    fix: authenticate password change requests

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -182,7 +182,7 @@ export class AuthController {
     return this.authService.createUserPassword(body, params);
   }
 
-  // @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard)
   @Post('change-password')
   @ApiBearerAuth('access_token')
   @ApiOkResponse({})
